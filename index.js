3b2eec68fc38156b2d3762f595844e3554f4a47a
ANTOINE1128 <makdessyantoine@gmail.com>
2023-05-09 12:00:19 +0300

    fix linters errors

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { DateTime } from './modules/luxon.js';
 import { Store } from './modules/storage.js';
 import { UI } from './modules/UI.js';
-import {displayContact ,displayForm , displayList} from './modules/display.js'
-
-
+import { displayContact, displayForm, displayList } from './modules/display.js';
 
 // Book Class: Represents a Book
 class Book {
@@ -13,10 +11,9 @@ class Book {
     this.isbn = isbn;
   }
 }
-
-// UI Class: Handle UI Tasks
-
-// Store Class: Handles Storage
+displayForm();
+displayContact();
+displayList();
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -52,8 +49,6 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   }
 });
 
-
-
 document.querySelector('#book-list').addEventListener('click', (e) => {
   // Remove book from UI
   UI.deleteBook(e.target);
@@ -68,9 +63,8 @@ let date = DateTime.fromObject({ day: 9, month: 5, year: 2023 });
 
 document.addEventListener('DOMContentLoaded', () => {
   const currentDate = document.getElementById('current-date');
-   date = date.toFormat('yyyy-MM-dd');
+  date = date.toFormat('yyyy-MM-dd');
   if (currentDate) {
     currentDate.textContent = date;
   }
 });
-
--- modules/display.js
+++ modules/display.js
@@ -1,21 +1,20 @@
-
- const displayList = () =>  {
+const displayList = () => {
   document.querySelector('#book-list').style.display = 'block';
   document.querySelector('#book-form').style.display = 'none';
   document.querySelector('#contact').style.display = 'none';
-}
+};
 
- const displayForm= () => {
+const displayForm = () => {
   document.querySelector('#book-list').style.display = 'none';
   document.querySelector('#book-form').style.display = 'block';
   document.querySelector('#contact').style.display = 'none';
-}
+};
 
- const displayContact = () => {
+const displayContact = () => {
   document.querySelector('#book-list').style.display = 'none';
   document.querySelector('#book-form').style.display = 'none';
   document.querySelector('#contact').style.display = 'block';
-}
+};
 document.querySelector('.list').addEventListener('click', () => {
   displayList();
 });
@@ -27,4 +26,4 @@ document.querySelector('.add').addEventListener('click', () => {
 document.querySelector('.contact-page').addEventListener('click', () => {
   displayContact();
 });
-export {displayContact ,displayForm , displayList}
+export { displayContact, displayForm, displayList };
